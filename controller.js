ea0be439e6c33fe7da066145ca190d4d01eaadc8
MgenGlder <mgen.glder@gmail.com>
2015-12-12 02:13:00 -0500

    Removed excess options from event calendar

--- controller.js
+++ controller.js
@@ -76,7 +76,7 @@ angular.module("app")
         height: 450,
         editable: true,
         header: {
-          left: 'month basicWeek basicDay agendaWeek agendaDay',
+          left: 'month agendaWeek agendaDay',
           center: 'title',
           right: 'today prev,next'
         },
